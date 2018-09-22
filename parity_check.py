3490e45d64fcb421635a1a6119c89bf66fd40b04
SheysSilva <sheilla.silva@ccc.ufcg.edu.br>
2018-09-22 14:01:32 -0300

    including tests

--- parity_check.py
+++ parity_check.py
@@ -1,8 +1,60 @@
-binary  = input()
+def creatingList(binary):
+    binary = binary.replace("0","0 ")
+    binary = binary.replace("1","1 ")
+    return binary.split()
 
-def paridade(binary:
+def verifyBinary(binary):
+    for bit in binary:
+        if(bit != '0' and bit != '1'):
+            return True
+    
+    return False
+
+def paridade(binary):
     count = 0
     for bit in binary:
-        if bit == "1":
-             count++
-    return count
+        if bit == '1':
+             count+=1
+    if((count%2) == 0):
+        return '0'
+    else:
+        return '1' 
+
+def removeParity(binary):
+    del binary[0]
+    return binary
+    
+def main(binary_receiver):
+    binary_receiver = creatingList(binary_receiver)
+    parity_receiver = binary_receiver[0]
+    new_binary_receiver = removeParity(binary_receiver)
+    new_parity_receiver = paridade(new_binary_receiver)
+
+    if(new_parity_receiver == parity_receiver):
+        return "Nenhum erro detectado"
+    else:
+        return "Erro detectado"
+
+def Test():
+    #Test One
+    binary_receiver_ok = ['101101011', '00101101', '00000', '1011111', '11111111']
+    binary_receiver_err = ['101101001', '00101111', '100000', '1011110', '01111111']
+
+    for i in range(5):
+        print("\n" + binary_receiver_ok[i] + ": \n" + main(binary_receiver_ok[i]))
+        print("\n" + binary_receiver_err[i] + ": \n" + main(binary_receiver_err[i]))
+
+    return 1    
+    
+Test()
+
+
+
+
+
+
+
+
+
+
+
